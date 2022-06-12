2edb31553e3915769cf8e7a857dde88957ba8b67
Kathryn Killebrew <kathryn.killebrew>
2022-06-12 01:24:53 -0400

    Set up view model

--- shoes/app/src/main/java/dev/killebrew/shoestore/ui/MainActivity.kt
+++ shoes/app/src/main/java/dev/killebrew/shoestore/ui/MainActivity.kt
@@ -2,12 +2,16 @@ package dev.killebrew.shoestore.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import dev.killebrew.shoestore.R
+import dev.killebrew.shoestore.models.ShoeViewModel
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val shoeViewModel: ShoeViewModel by viewModels()
     }
 }
