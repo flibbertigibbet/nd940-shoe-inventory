a29e737492729fa0645effedc2b2979f37a5705b
Kathryn Killebrew <kathryn.killebrew>
2022-06-12 21:00:01 -0400

    Use data binding in main activity

--- shoes/app/src/main/java/dev/killebrew/shoestore/ui/MainActivity.kt
+++ shoes/app/src/main/java/dev/killebrew/shoestore/ui/MainActivity.kt
@@ -3,17 +3,21 @@ package dev.killebrew.shoestore.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import dev.killebrew.shoestore.R
+import dev.killebrew.shoestore.databinding.ActivityMainBinding
 import dev.killebrew.shoestore.models.ShoeViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val shoeViewModel: ShoeViewModel by viewModels()
 
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(binding.toolbar)
     }
 }
